2ab5fdd2f5a044025c523aa73215f535f9ee4abe
grichard29 <guillaume.richard29@live.fr>
2016-09-06 19:29:15 +0800

    first

---
+++ contactus.php
@@ -0,0 +1,32 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head> <h2>CONTACT US</h2></head>
+
+<body>
+<form action="/ma-page-de-traitement" method="post">
+    <div>
+        <label for="nom">Nom :</label>
+        <input type="text" id="nom" />
+    </div>
+    <div>
+        <label for="courriel">Courriel :</label>
+        <input type="email" id="courriel" />
+    </div>
+    <div>
+        <label for="message">Message :</label>
+        <textarea id="message"></textarea>
+    </div>
+    
+    <div class="button">
+        <button type="submit">Envoyer votre message</button>
+    </div>
+</form>
+</body>
+
+</html>
+
+<?php
+
+
+?>
